ae9f19f4f2f9150cfa71df82f45b015864895cb2
Martin Minkov <minkovlmartin@gmail.com>
2022-05-10 13:51:21 -0700

    feat: Add gtm to _document.js (#138)

--- pages/_document.js
+++ pages/_document.js
@@ -28,8 +28,23 @@ export default class MyDocument extends Document {
       <Html style={globalStyles}>
         <Head>
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KCQRV4Z');`,
+            }}
+          ></script>
         </Head>
         <body style={{ margin: 0 + "px" }}>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCQRV4Z"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
